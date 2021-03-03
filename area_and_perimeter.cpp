ac1e5cd5822c6289cf651c84807b477709bc7ded
Patrick Coxall <mr.coxall@mths.ca>
2021-03-03 15:56:34 +0000

    fixed input

--- area_and_perimeter.cpp
+++ area_and_perimeter.cpp
@@ -1,16 +1,16 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This program calculates the area and perimeter of a circle
-//    with radius 5cm
+//    with radius 15mm
 
 #include <iostream>
 #include <cmath>
 
 int main() {
-	std::cout << "If a circle has a radius of 5cm: " << std::endl;
+	std::cout << "If a circle has a radius of 15mm: " << std::endl;
     std::cout << std::endl;
-    std::cout << "Area is " << (M_PI * pow(5, 2)) << "cm²." << std::endl;
-    std::cout << "Perimeter is " << (2 * M_PI * 5) << "cm." << std::endl;
+    std::cout << "Area is " << (M_PI * pow(15, 2)) << "mm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * M_PI * 15) << "mm." << std::endl;
     std::cout << std::endl;
     std::cout << "Done." << std::endl;
 }
