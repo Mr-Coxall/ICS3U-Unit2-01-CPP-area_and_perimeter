c556b045d45bd80919e7086d558da7162c7b7dca
Patrick Coxall <mr.coxall@mths.ca>
2021-04-26 03:19:28 +0000

    fixed units

--- area_and_perimeter.cpp
+++ area_and_perimeter.cpp
@@ -10,11 +10,11 @@
 
 int main() {
     // this function calculates the area and perimeter
-    
-	std::cout << "If a circle has a radius of 15mm: " << std::endl;
+
+    std::cout << "If a circle has a radius of 15mm: " << std::endl;
     std::cout << std::endl;
-    std::cout << "Area is " << (M_PI * pow(15, 2)) << "mm²." << std::endl;
-    std::cout << "Perimeter is " << (2 * M_PI * 15) << "mm." << std::endl;
+    std::cout << "Area is " << (M_PI * pow(15, 2)) << " mm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * M_PI * 15) << " mm." << std::endl;
     std::cout << std::endl;
     std::cout << "Done." << std::endl;
 }
